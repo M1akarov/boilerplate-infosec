6de78223f6175ba18968c7147e38dc34ed6e9b6f
yevhenii <dodiak2021@gmail.com>
2025-06-08 23:05:53 -0400

    Отключил DNS-prefetching с помощью helmet.dnsPrefetchControl()

--- myApp.js
+++ myApp.js
@@ -8,6 +8,7 @@ app.use(helmet.frameguard({ action: 'deny' }));
 app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
+app.use(helmet.dnsPrefetchControl());
 
 const ninetyDaysInSeconds = 90 * 24 * 60 * 60;
 app.use(helmet.hsts({
